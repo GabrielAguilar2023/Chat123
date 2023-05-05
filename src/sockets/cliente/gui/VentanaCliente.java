a796ba3d8bc4739815266b101ebcd5d011703c91
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-05-05 01:44:02 -0300

    Modificaciones menores

--- src/sockets/cliente/gui/VentanaCliente.java
+++ src/sockets/cliente/gui/VentanaCliente.java
@@ -6,11 +6,12 @@ import java.awt.*;
 public class VentanaCliente extends JFrame {
 
     public VentanaCliente() throws HeadlessException {
-        setBounds(600, 300, 250, 350);
+        setTitle("Chat123");
+        setBounds(600, 200, 240, 320);
         setAlwaysOnTop(true);         // para que siempre este visible en primer plano
         setLocationRelativeTo(null);  // para que aparezca centrada en la pantalla
-      //  setUndecorated(true);         // quita el decorado para poder utilizar la opacidad
-      //  setOpacity(0.9f);             // porcentaje de transparencia
+     //   setUndecorated(true);         // quita el decorado para poder utilizar la opacidad
+     //   setOpacity(0.8f);             // porcentaje de transparencia
         PanelCliente ContenedorDentroVentana = new PanelCliente();
         add(ContenedorDentroVentana);
         setVisible(true);
